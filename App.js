475ec38ccc44d3b16935cb69f888ae0052188b45
selva-1201 <kselva120102@gmail.com>
2024-12-30 08:59:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import TextToSpeech from './components/TextToSpeech'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TextToSpeech />
+    </div>
+  )
+}
+
+export default App;
